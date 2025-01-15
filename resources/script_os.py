29cf10919d42057e4e62fad90bb6d169ae4e6fc0
Yaroslav Gusev <yarigusev@gmail.com>
2025-01-15 11:49:10 +0300

    corrects

--- resources/script_os.py
+++ resources/script_os.py
@@ -2,7 +2,7 @@ import os.path
 
 CURRENT_FILE = os.path.abspath(__file__)
 CURRENT_DIR = os.path.dirname(CURRENT_FILE)
-TMP_DIR = os.path.join(CURRENT_DIR, 'resources', "temp")
+TMP_DIR = os.path.join(CURRENT_DIR, "temp")
 
 if not os.path.exists("temp"):
     os.mkdir("temp")
